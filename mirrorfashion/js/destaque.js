05638d73a9a25b409905fb87cf932450e4c92377
tafnes <rebecatafnes@hotmail.com>
2015-05-29 17:46:17 -0300

    add js

--- mirrorfashion/js/destaque.js
+++ mirrorfashion/js/destaque.js
@@ -2,20 +2,22 @@ var banners = ["img/destaque-home.png", "img/destaque-home-2.png"];
 var bannerAtual = 0;
 
 function trocaBanner() {
-	bannerAtual = (bannerAtual +1 ) %2;
-	document.querySelector('.destaque img').src = [bannerAtual]
+
+	bannerAtual = (bannerAtual + 1) %2;
+	document.querySelector('.destaque img').src = banners[bannerAtual];
+
 }
-setInterval (trocaBanner, 4000);
+var timer = setInterval(trocaBanner, 4000);
 
 var controle = document.querySelector('.pause');
 
 controle.onclick = function() {
-	if (controle.className == 'pause'") {
+	if (controle.className == 'pause') {
 		clearInterval(timer);
-		controle.className = 'play'
+		controle.className = 'play';
 	} else {
 		timer = setInterval(trocaBanner, 4000);
-		controle.className = 'pause'
+		controle.className = 'pause';
 	}
 
 	return false;
